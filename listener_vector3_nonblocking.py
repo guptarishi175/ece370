65e64a05b602ab051961def402ac432ba8dbf1d0
Rishi Gupta <rgupta14@gmu.edu>
2018-04-07 12:18:48 -0400

    Add listener file

---
+++ listener_vector3_nonblocking.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Vector3
+
+pub_data = Vector3()
+
+def callback(data):
+    global pub_data
+    pub_data = data
+    rospy.loginfo("x = " + str(data.x) + " y = " + str(data.y))
+    
+def listener():
+    global pub_data
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('camera__listener', anonymous=True)
+
+    rospy.Subscriber("camera", Vector3, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+#    rospy.spin()
+
+
+    rate = rospy.Rate(30) # 30hz
+    while not rospy.is_shutdown():
+        rospy.loginfo(" --- OLD --- x = " + str(pub_data.x) + " y = " + str(pub_data.y))
+        rate.sleep()
+
+if __name__ == '__main__':
+    listener()
