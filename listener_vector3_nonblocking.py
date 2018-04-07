751f81fe200fa1a1ea1c68d6297125f7af9381ae
Rishi Gupta <rgupta14@gmu.edu>
2018-04-07 13:21:14 -0400

    fixed listener

--- listener_vector3_nonblocking.py
+++ listener_vector3_nonblocking.py
@@ -16,9 +16,9 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('camera__listener', anonymous=True)
+    rospy.init_node('camera_listener', anonymous=True)
 
-    rospy.Subscriber("camera", Vector3, callback)
+    rospy.Subscriber("centerCoord", Vector3, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
 #    rospy.spin()
