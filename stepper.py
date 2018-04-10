4dd89dd45eae2a8d9b5a2f3c57724ed09e35d6bc
Rishi Gupta <rgupta14@gmu.edu>
2018-04-09 22:35:59 -0400

    added listener feature to stepper code

--- stepper.py
+++ stepper.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+import rospy
+from geometry_msgs.msg import Vector3
 import sys
+
+pub_data = Vector3()
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -43,4 +47,24 @@ def motorControl(motor, wheel_directon):
 		for pin in range(4):
 			GPIO.output(motor[pin], wheel_direction[halfstep][pin])
 		time.sleep(0.001)
+
+
+def callback(data):
+	global pub_data
+	pub_data = data
+	rospy.loginfo("x = " + str(data.x) + " y = " + str(data.y))
+
+def listener():
+	global pub_data
+	rospy.init_node('camera_listener', anonymous = True)
+	rospy.Subscriber('centerCoord', Vector3, callback)
+	rate = rospy.Rate(30)
+	while not rospy.is_shutdown():
+		rospy.loginfo("--- OLD --- x = " + str(pub_data.x) + " y = " + str(pub_data.y))
+		rate.sleep()
+
+if __name__ == '__main__':
+	listener()
+
+
 GPIO.cleanup()
